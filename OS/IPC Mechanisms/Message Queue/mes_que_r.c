11fc6497403012f5ce7a99d5b5202b03d01d03f0
20WH1A1219 <96655092+20WH1A1219@users.noreply.github.com>
2022-06-05 06:10:34 +0530

    Update mes_que_r.c

--- OS/IPC Mechanisms/Message Queue/mes_que_r.c
+++ OS/IPC Mechanisms/Message Queue/mes_que_r.c
@@ -10,23 +10,10 @@ int main()
 {
     key_t key;
     int msgid;
-  
-    // ftok to generate unique key
     key = ftok("progfile", 65);
-  
-    // msgget creates a message queue
-    // and returns identifier
     msgid = msgget(key, 0666 | IPC_CREAT);
-    // msgrcv to receive message
     msgrcv(msgid, &message, sizeof(message), 1, 0);
-  
-    // display the message
-    printf("Data Received is : %s \n", 
-                    message.mesg_text);
-  
-    // to destroy the message queue
+    printf("Data Received is : %s \n",message.mesg_text);
     msgctl(msgid, IPC_RMID, NULL);
-  
     return 0;
 }
-
