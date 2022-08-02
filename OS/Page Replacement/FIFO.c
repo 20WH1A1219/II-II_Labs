7c8fecef42eb6414b6e016cf661d4e9be8387586
20WH1A1219 <96655092+20WH1A1219@users.noreply.github.com>
2022-08-02 22:42:28 +0530

    Update FIFO.c

--- OS/Page Replacement/FIFO.c
+++ OS/Page Replacement/FIFO.c
@@ -1,125 +1,50 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
-#include <unistd.h>
-
-#define N 5
-#define THINKING 2
-#define HUNGRY 1
-#define EATING 0
-#define LEFT (phnum + 4) % N
-#define RIGHT (phnum + 1) % N
-
-int state[N];
-int phil[N] = { 0, 1, 2, 3, 4 };
-
-sem_t mutex;
-sem_t S[N];
-
-void test(int phnum)
-{
-	if (state[phnum] == HUNGRY
-		&& state[LEFT] != EATING
-		&& state[RIGHT] != EATING) {
-		// state that eating
-		state[phnum] = EATING;
-
-		sleep(2);
-
-		printf("Philosopher %d takes fork %d and %d\n",
-					phnum + 1, LEFT + 1, phnum + 1);
-
-		printf("Philosopher %d is Eating\n", phnum + 1);
-
-		// sem_post(&S[phnum]) has no effect
-		// during takefork
-		// used to wake up hungry philosophers
-		// during putfork
-		sem_post(&S[phnum]);
-	}
-}
-
-// take up chopsticks
-void take_fork(int phnum)
-{
-
-	sem_wait(&mutex);
-
-	// state that hungry
-	state[phnum] = HUNGRY;
-
-	printf("Philosopher %d is Hungry\n", phnum + 1);
-
-	// eat if neighbours are not eating
-	test(phnum);
-
-	sem_post(&mutex);
-
-	// if unable to eat wait to be signalled
-	sem_wait(&S[phnum]);
-
-	sleep(1);
-}
-
-// put down chopsticks
-void put_fork(int phnum)
-{
-
-	sem_wait(&mutex);
-
-	// state that thinking
-	state[phnum] = THINKING;
-
-	printf("Philosopher %d putting fork %d and %d down\n",
-		phnum + 1, LEFT + 1, phnum + 1);
-	printf("Philosopher %d is thinking\n", phnum + 1);
-
-	test(LEFT);
-	test(RIGHT);
-
-	sem_post(&mutex);
-}
-
-void* philosopher(void* num)
-{
-
-	while (1) {
-
-		int* i = num;
-
-		sleep(1);
-
-		take_fork(*i);
-
-		sleep(0);
-
-		put_fork(*i);
-	}
-}
-
 int main()
 {
-
-	int i;
-	pthread_t thread_id[N];
-
-	// initialize the semaphores
-	sem_init(&mutex, 0, 1);
-
-	for (i = 0; i < N; i++)
-
-		sem_init(&S[i], 0, 0);
-
-	for (i = 0; i < N; i++) {
-
-		// create philosopher processes
-		pthread_create(&thread_id[i], NULL,
-					philosopher, &phil[i]);
-
-		printf("Philosopher %d is thinking\n", i + 1);
-	}
-
-	for (i = 0; i < N; i++)
-
-		pthread_join(thread_id[i], NULL);
+int referenceString[10], pageFaults = 0, m, n, s, pages, frames;
+printf("\nEnter the number of Pages:\t");
+scanf("%d", &pages);
+printf("\nEnter reference string values:\n");
+for( m = 0; m < pages; m++)
+{
+printf("Value No. [%d]:\t", m + 1);
+scanf("%d", &referenceString[m]);
+}
+printf("\n What are the total number of frames:\t");
+{
+scanf("%d", &frames);
+}
+int temp[frames];
+for(m = 0; m < frames; m++)
+{
+temp[m] = -1;
+}
+for(m = 0; m < pages; m++)
+{
+s = 0;
+for(n = 0; n < frames; n++)
+{
+if(referenceString[m] == temp[n])
+{
+s++;
+pageFaults--;
+}
+}
+pageFaults++;
+if((pageFaults <= frames) && (s == 0))
+{
+temp[m] = referenceString[m];
+}
+else if(s == 0)
+{
+temp[(pageFaults - 1) % frames] = referenceString[m];
+}
+printf("\n");
+for(n = 0; n < frames; n++)
+{
+printf("%d\t", temp[n]);
+}
+}
+printf("\nTotal Page Faults:\t%d\n", pageFaults);
+return 0;
 }
